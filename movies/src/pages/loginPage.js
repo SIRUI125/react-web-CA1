864bef787ddee6862c049c769e562602d079b2ff
Sirui Yao <20095250@mail.wit.ie>
2023-11-16 18:34:46 +0000

    update login page

--- movies/src/pages/loginPage.js
+++ movies/src/pages/loginPage.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../components/firebase/firebase'; 
 import { useNavigate } from 'react-router-dom';
-import '../css/login.css';
+import {
+  Button, TextField, Box, Card, CardContent, Typography, CssBaseline, 
+  Avatar, Container, Paper, Grid
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
@@ -14,7 +18,7 @@ const LoginPage = () => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
       alert("Login successful!");
-      navigate('/'); // Redirect to home page after login
+      navigate('/');
     } catch (error) {
       console.error(error.message);
     }
@@ -25,38 +29,91 @@ const LoginPage = () => {
     try {
       await createUserWithEmailAndPassword(auth, email, password);
       alert("Registration successful!");
-      navigate('/'); // Redirect to home page after registration
+      navigate('/');
     } catch (error) {
       console.error(error.message);
     }
   };
 
   return (
-    <div className="login-container">
-      <h2>Sign up/Login</h2>
-      <form onSubmit={handleLogin}>
-        <label>
-          Email:
-          <input 
-            type="email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-            required 
-          />
-        </label>
-        <label>
-          Password:
-          <input 
-            type="password" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-            required 
-          />
-        </label>
-        <button type="submit">Login</button>
-        <button onClick={handleSignUp}>Sign Up</button>
-      </form>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Paper 
+        sx={{ 
+          mt: 8, 
+          display: 'flex', 
+          flexDirection: 'column', 
+          alignItems: 'center',
+          padding: 2
+        }}
+      >
+        <Card sx={{ backgroundColor: '#f5f5f5' }}>
+          <CardContent>
+            <Box
+              sx={{
+                my: 3,
+                mx: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5" color="primary">
+                Sign in
+              </Typography>
+              <Box component="form" noValidate onSubmit={handleLogin} sx={{ mt: 1, width: '100%' }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={6}>
+                    <TextField
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoComplete="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="current-password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </Grid>
+                </Grid>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="secondary"
+                  onClick={handleSignUp}
+                  sx={{ mt: 1 }}
+                >
+                  Sign Up
+                </Button>
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      </Paper>
+    </Container>
   );
 };
 
